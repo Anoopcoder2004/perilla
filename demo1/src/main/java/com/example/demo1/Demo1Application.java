bae8dd6f4328305b5d810e47edb8aec0ed4a65a5
Anoop <Anooppopziyan@gmail.com>
2025-10-16 14:57:11 +0530

    first api get user

--- demo1/src/main/java/com/example/demo1/Demo1Application.java
+++ demo1/src/main/java/com/example/demo1/Demo1Application.java
@@ -5,12 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 @RestController
 public class Demo1Application {
     @GetMapping("/hello")
     public String hello(){
         return "hello world";
+    }
+    @GetMapping("/user")
+    public Map<String,String>getUser(){
+        Map<String,String> user = new HashMap<>();
+        user.put("name","Anoop");
+        user.put("role","developer");
+        return user;
+
     }
 
 	public static void main(String[] args) {
